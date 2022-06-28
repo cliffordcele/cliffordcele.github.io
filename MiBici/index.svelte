681751f00e6f090aef91d40b00e6d1945ddbf021
Clifford Cele <delanicele@live.com>
2022-06-28 19:05:14 -0400

    add index page

---
+++ MiBici/index.svelte
@@ -0,0 +1,6 @@
+<h1>Custom visualizations</h1>
+<p>
+  Earlier in the course, you created custom designs to visualize an energy
+  consumption dataset. We have picked three of your designs for you to implement
+  in the final assignment.
+</p>
